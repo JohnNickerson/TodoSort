391407538a2025a54b50750fe10706b9f91922e3
nickersonj <nickersonj@users.sourceforge.net>
2010-05-27 16:46:32 +1000

    Factored out file reading and writing to static methods on the Item class. Restored Someday file sorting, which also simplified the Defer method.

--- Item.cs
+++ Item.cs
@@ -2,13 +2,109 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace TodoSort
 {
     class Item
     {
+        #region Fields
         public string Text;
         public string Context;
-        public List<string> SubItems = new List<string>();
+        public List<string> SubItems;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructs a new Item with a given context, title and optional sub-items.
+        /// </summary>
+        /// <param name="context">The context to which this item belongs.</param>
+        /// <param name="title">The text of the action item.</param>
+        /// <param name="subitems">Optional list of sub-items to go under this one.</param>
+        public Item(string context, string title, params string[] subitems)
+        {
+            this.Context = context;
+            this.Text = title;
+            this.SubItems = new List<string>(subitems);
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Reads a formatted file and returns it as a list of Items.
+        /// </summary>
+        /// <param name="filename">The full path to the file to load.</param>
+        /// <returns>A list of items as represented by the file.</returns>
+        public static List<Item> ReadFile(string filename)
+        {
+            string[] items = File.ReadAllLines(filename);
+
+            List<Item> allitems = new List<Item>();
+            string context = string.Empty;
+            Item curitem = new Item(context, "(item out of order)");
+            for (int x = 0; x < items.Length; x++)
+            {
+                if (items[x].StartsWith("@"))
+                {
+                    // New context.
+                    context = items[x];
+                }
+                else if (items[x].StartsWith("\t\t"))
+                {
+                    // Sub-item.
+                    curitem.SubItems.Add(items[x]);
+                }
+                else
+                {
+                    // New item.
+                    curitem = new Item(context, items[x]);
+                    allitems.Add(curitem);
+                }
+            }
+
+            return allitems;
+        }
+        /// <summary>
+        /// Writes a list of items out to a file.
+        /// </summary>
+        /// <param name="filename">The full path of the file to write.</param>
+        /// <param name="items">The items to write out.</param>
+        /// <param name="includeinbox">True to add an "@inbox" context at the end, false to leave it out unless already present.</param>
+        public static void WriteToFile(string filename, List<Item> items, bool includeinbox)
+        {
+            File.Delete(filename);
+            foreach (string b in (from s in items orderby s.Context select s.Context).Distinct())
+            {
+                if (!b.Equals("@inbox"))
+                {
+                    // Write out block.
+                    File.AppendAllText(filename, b);
+                    File.AppendAllText(filename, Environment.NewLine);
+                    foreach (Item i in from t in items orderby t.Text where t.Context == b select t)
+                    {
+                        File.AppendAllText(filename, i.Text);
+                        File.AppendAllText(filename, Environment.NewLine);
+                        foreach (string sub in i.SubItems)
+                        {
+                            File.AppendAllText(filename, sub);
+                            File.AppendAllText(filename, Environment.NewLine);
+                        }
+                    }
+                }
+            }
+            if (includeinbox)
+            {
+                // Write out inbox.
+                File.AppendAllText(filename, "@inbox");
+                File.AppendAllText(filename, Environment.NewLine);
+                foreach (Item i in from t in items orderby t.Text where t.Context == "@inbox" select t)
+                {
+                    File.AppendAllText(filename, i.Text);
+                    File.AppendAllText(filename, Environment.NewLine);
+                    // Sub-items not supported for inbox.
+                }
+            }
+        }
+        #endregion
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,173 +11,112 @@ namespace TodoSort
     {
         static void Main(string[] args)
         {
-            foreach (string filename in new string[] {
-                ConfigurationManager.AppSettings["todo"]
-            })
-            {
-                #region Deserialise the file
-                string[] items = File.ReadAllLines(filename);
+            // Deserialise the files
+            List<Item> todolist = Item.ReadFile(ConfigurationManager.AppSettings["todo"]);
+            List<Item> someday = Item.ReadFile(ConfigurationManager.AppSettings["someday"]);
 
-                List<Item> allitems = new List<Item>();
-                string context = string.Empty;
-                Item curitem = new Item();
-                curitem.Text = "(item out of order)";
-                for (int x = 0; x < items.Length; x++)
+            #region Manipulate the file items
+            if (args.Length > 0)
+            {
+                string command = args[0];
+                // Search for a matching item in all contexts.
+                Item selected = null;
+                if (args.Length > 1)
                 {
-                    if (items[x].StartsWith("@"))
+                    var matches = from i in todolist
+                                  where i.Text.Contains(args[1])
+                                  select i;
+                    // Disambiguate if required.
+                    if (matches.Count() == 1)
                     {
-                        // New context.
-                        context = items[x];
+                        selected = matches.First();
                     }
-                    else if (items[x].StartsWith("\t\t"))
+                    else if (matches.Count() > 5)
                     {
-                        // Sub-item.
-                        curitem.SubItems.Add(items[x]);
+                        Console.WriteLine("Too many search matches. Try to be more specific. No action will be taken this time.");
                     }
                     else
                     {
-                        // New item.
-                        curitem = new Item();
-                        curitem.Text = items[x];
-                        curitem.Context = context;
-                        allitems.Add(curitem);
+                        for (int i = 0; i < matches.Count(); i++)
+                        {
+                            Console.WriteLine("{0}: {1}", i, matches.ElementAt(i).Text);
+                        }
+                        char choice = Console.ReadKey().KeyChar;
+                        int dex = Int32.Parse(choice.ToString());
+                        selected = matches.ElementAt(dex);
                     }
                 }
-                #endregion
-
-                #region Manipulate the file items
-                if (args.Length > 0)
+                switch (command)
                 {
-                    string command = args[0];
-                    // Search for a matching item in all contexts.
-                    Item selected = null;
-                    if (args.Length > 1)
-                    {
-                        var matches = from i in allitems
-                                      where i.Text.Contains(args[1])
-                                      select i;
-                        // Disambiguate if required.
-                        if (matches.Count() == 1)
+                    case "process":
+                        // Go over the @inbox items and assign them to contexts.
+                        for (int i = 0; i < todolist.Count; i++)
                         {
-                            selected = matches.First();
-                        }
-                        else if (matches.Count() > 5)
-                        {
-                            Console.WriteLine("Too many search matches. Try to be more specific. No action will be taken this time.");
-                        }
-                        else
-                        {
-                            for (int i = 0; i < matches.Count(); i++)
+                            if (todolist[i].Context == "@inbox")
                             {
-                                Console.WriteLine("{0}: {1}", i, matches.ElementAt(i).Text);
-                            }
-                            char choice = Console.ReadKey().KeyChar;
-                            int dex = Int32.Parse(choice.ToString());
-                            selected = matches.ElementAt(dex);
-                        }
-                    }
-                    switch (command)
-                    {
-                        case "process":
-                            // Go over the @inbox items and assign them to contexts.
-                            for (int i = 0; i < allitems.Count; i++)
-                            {
-                                if (allitems[i].Context == "@inbox")
+                                Console.WriteLine("To which context should this item go?");
+                                Item first = todolist[i];
+                                Console.WriteLine(first.Text);
+                                string newcontext = Console.ReadLine();
+                                if (!newcontext.StartsWith("@"))
                                 {
-                                    Console.WriteLine("To which context should this item go?");
-                                    Item first = allitems[i];
-                                    Console.WriteLine(first.Text);
-                                    string newcontext = Console.ReadLine();
-                                    if (!newcontext.StartsWith("@"))
+                                    newcontext = "@" + newcontext;
+                                }
+                                Console.WriteLine();
+                                if (newcontext.Equals("someday"))
+                                {
+                                    Defer(todolist, someday, first);
+                                }
+                                else
+                                {
+                                    first.Context = newcontext;
+                                    if (!first.Text.StartsWith("\t"))
                                     {
-                                        newcontext = "@" + newcontext;
-                                    }
-                                    Console.WriteLine();
-                                    if (newcontext.Equals("someday"))
-                                    {
-                                        Defer(allitems, first);
-                                    }
-                                    else
-                                    {
-                                        first.Context = newcontext;
-                                        if (!first.Text.StartsWith("\t"))
-                                        {
-                                            first.Text = "\t" + first.Text;
-                                        }
+                                        first.Text = "\t" + first.Text;
                                     }
                                 }
                             }
-                            break;
-                        case "defer":
-                            // Move the selected item and its sub-items to the "someday" file.
-                            Defer(allitems, selected);
-                            break;
-                        case "done":
-                            // If there is a next action, create a new item and add it to the correct context.
-                            if (selected != null)
-                            {
-                                if (selected.SubItems.Count > 0 && selected.SubItems[0].Trim().StartsWith("&@"))
-                                {
-                                    Item next = new Item();
-                                    next.SubItems = selected.SubItems;
-                                    string newcontext = next.SubItems[0].Split(' ')[0].Trim().Remove(0, 1);
-                                    next.Text = next.SubItems[0].Remove(1, 2 + newcontext.Length + 1);
-                                    next.SubItems.RemoveAt(0);
-                                    allitems.Add(next);
-                                }
-                                // Log the completed action to the Done file.
-                                WriteToFile("done", string.Format("{0}: {1}", DateTime.Now, selected.Text));
-                                allitems.Remove(selected);
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                #endregion
-
-                #region Rewrite the file
-                File.Delete(filename);
-                foreach (string b in (from s in allitems orderby s.Context select s.Context).Distinct())
-                {
-                    if (!b.Equals("@inbox"))
-                    {
-                        // Write out block.
-                        File.AppendAllText(filename, b);
-                        File.AppendAllText(filename, Environment.NewLine);
-                        foreach (Item i in from t in allitems orderby t.Text where t.Context == b select t)
-                        {
-                            File.AppendAllText(filename, i.Text);
-                            File.AppendAllText(filename, Environment.NewLine);
-                            foreach (string sub in i.SubItems)
-                            {
-                                File.AppendAllText(filename, sub);
-                                File.AppendAllText(filename, Environment.NewLine);
-                            }
                         }
-                    }
+                        break;
+                    case "defer":
+                        // Move the selected item and its sub-items to the "someday" file.
+                        Defer(todolist, someday, selected);
+                        break;
+                    case "done":
+                        // If there is a next action, create a new item and add it to the correct context.
+                        if (selected != null)
+                        {
+                            if (selected.SubItems.Count > 0 && selected.SubItems[0].Trim().StartsWith("&@"))
+                            {
+                                Item next = new Item(string.Empty, string.Empty);
+                                next.SubItems = selected.SubItems;
+                                string newcontext = next.SubItems[0].Split(' ')[0].Trim().Remove(0, 1);
+                                next.Text = next.SubItems[0].Remove(1, 2 + newcontext.Length + 1);
+                                next.SubItems.RemoveAt(0);
+                                todolist.Add(next);
+                            }
+                            // Log the completed action to the Done file.
+                            WriteToFile("done", string.Format("{0}: {1}", DateTime.Now, selected.Text));
+                            todolist.Remove(selected);
+                        }
+                        break;
+                    default:
+                        break;
                 }
-                // Write out inbox.
-                File.AppendAllText(filename, "@inbox");
-                File.AppendAllText(filename, Environment.NewLine);
-                foreach (Item i in from t in allitems orderby t.Text where t.Context == "@inbox" select t)
-                {
-                    File.AppendAllText(filename, i.Text);
-                    File.AppendAllText(filename, Environment.NewLine);
-                    // Sub-items not supported for inbox.
-                }
-                #endregion
             }
+            #endregion
+           
+
+            // Rewrite the files
+            Item.WriteToFile(ConfigurationManager.AppSettings["todo"], todolist, true);
+            // Sort the Someday file.
+            Item.WriteToFile(ConfigurationManager.AppSettings["someday"], someday, false);
         }
 
-        private static void Defer(List<Item> allblocks, Item selected)
+        private static void Defer(List<Item> from, List<Item> to, Item selected)
         {
-            WriteToFile("someday", selected.Text);
-            foreach (string s in selected.SubItems)
-            {
-                WriteToFile("someday", s);
-            }
-            allblocks.Remove(selected);
+            to.Add(selected);
+            from.Remove(selected);
         }
 
         /// <summary>
