1ce1cf17f6f7a15cf0d498db577130ca9187649b
nickersonj <nickersonj@users.sourceforge.net>
2010-12-21 16:31:00 +1000

    Added project settings file. Added an experimental WPF GUI project.

---
+++ WpfGui/Window1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGui
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+
+			// Test layout.
+
+			// foreach Context in todolist
+			// Add a TreeViewItem with Header=@Context
+			// foreach Item in Context
+			// Add a TreeViewItem under the Context item.
+			// Add a Checkbox to the TreeViewItem with Content=Item Text
+
+		}
+	}
+}
