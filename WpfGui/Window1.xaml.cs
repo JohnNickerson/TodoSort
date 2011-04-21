7a54077716438918d7dde1547e880f5ca2c094a2
nickersonj <nickersonj@users.sourceforge.net>
2011-04-21 15:25:40 +1000

    Added rudimentary action list display based on file contents.

--- WpfGui/Window1.xaml.cs
+++ WpfGui/Window1.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AssimilationSoftware.PimData;
+using WpfGui.Properties;
 
 namespace WpfGui
 {
@@ -19,18 +21,40 @@ namespace WpfGui
 	/// </summary>
 	public partial class Window1 : Window
 	{
+        private List<ActionItem> _todolist = ActionItem.ReadFile(Settings.Default.TodoFilename);
+
 		public Window1()
 		{
 			InitializeComponent();
 
+
 			// Test layout.
-
+            todotree.Items.Clear();
 			// foreach Context in todolist
-			// Add a TreeViewItem with Header=@Context
-			// foreach Item in Context
-			// Add a TreeViewItem under the Context item.
-			// Add a Checkbox to the TreeViewItem with Content=Item Text
+            foreach (var c in (from a in _todolist orderby a.Context select a.Context).Distinct())
+            {
+                // Add a TreeViewItem with Header=@Context
+                TreeViewItem context = new TreeViewItem();
+                context.Header = c;
+                todotree.Items.Add(context);
+                // foreach Item in Context
+                foreach (var t in (from a in _todolist where a.Context == c orderby a.Title select a))
+                {
+                    // Add a Checkbox to the TreeViewItem with Content=Item Text
+                    CheckBox item = new CheckBox();
+                    context.Items.Add(item);
+                    item.Content = t.Title;
+                    item.Tag = t;
 
+                    item.Checked += new RoutedEventHandler(item_Checked);
+                }
+            }
 		}
+
+        void item_Checked(object sender, RoutedEventArgs e)
+        {
+            ActionItem item = (ActionItem)((CheckBox)sender).Tag;
+            //item.Done();
+        }
 	}
 }
