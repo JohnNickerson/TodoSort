563f5ebbe0cafb22474332f6c3d8904a646c1450
John <mokalusofborg@gmail.com>
2017-06-14 14:04:56 +1000

    Added extension methods to support tree balancing.

---
+++ Core/ActionItem_Extensions.cs
@@ -0,0 +1,43 @@
+﻿using AssimilationSoftware.PimData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssimilationSoftware.TodoSort.Core
+{
+    public static class ActionItem_Extensions
+    {
+        public static int GetIntTag(this ActionItem item, string tagname, int fallback)
+        {
+            if (item == null)
+            {
+                return fallback;
+            }
+            else if (item.Tags.ContainsKey(tagname))
+            {
+                int val = fallback;
+                var success = int.TryParse(item.Tags[tagname], out val);
+                if (success)
+                {
+                    return val;
+                }
+                else
+                {
+                    return fallback;
+                }
+            }
+            return fallback;
+        }
+
+        public static int Depth(this ActionItem item)
+        {
+            if (item.RankParent == null)
+            {
+                return 0;
+            }
+            return 1 + item.Depth();
+        }
+    }
+}
