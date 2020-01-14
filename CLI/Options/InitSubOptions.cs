65f1bef240524484363eb05ee4d911c1c6b74bf2
John <mokalusofborg@gmail.com>
2020-01-14 10:01:22 +1000

    Removed long-obsolete "someday" and "done" options from the "init" CLI command.

--- CLI/Options/InitSubOptions.cs
+++ CLI/Options/InitSubOptions.cs
@@ -10,11 +10,5 @@ namespace AssimilationSoftware.TodoSort.CLI.Options
     {
         [Option('f', "filename", HelpText = "The main action list file name.", Required = true)]
         public string TodoFile { get; set; }
-
-        [Option("someday", HelpText = "The file name for deferred items.")]
-        public string SomedayFile { get; set; }
-
-        [Option("done", HelpText = "The file name for completed items.")]
-        public string DoneFile { get; set; }
     }
 }
